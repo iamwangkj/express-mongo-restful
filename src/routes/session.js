0bcd5e8c73e4df00e08f2b7574ba2d20e27c0565
wangkj <ne.wkj@qq.com>
2020-01-15 00:04:24 +0800

    init project

--- src/routes/session.js
+++ src/routes/session.js
@@ -1,26 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const mongodb = require('mongodb')
-// const mysqlConfig = require('../config/db.js')
-
-// 查询语句
-const db = {
-  query: 'select * from user where username=?',
-  // insert: 'insert into user(id, name, age) value(0,?,?)',
-  // queryAll: 'select * from user',
-  // update: 'update user set name=?, age=? where id=?',
-  // delete: 'delete from user where id=?'
-}
-
-const mongoClient = mongodb.MongoClient
+const mongoClient = require('mongodb').MongoClient
 
 
 router.post('/', (req, res, next) => {
-  let { username, password } = req.body
-  mongodb.connect('mogodb://localhost:27017/express', {useNewUrlParser: true}, (err, result) => {
-    if(err) throw err
-    
-  })
+  const { username, password } = req.body
 })
 
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,71 +1,64 @@
 const express = require('express')
 const router = express.Router()
-const mysql = require('mysql')
-const mysqlConfig = require('../config/db.js')
+const mongoClient = require('mongodb').MongoClient
+const dbUrl = 'mongodb://localhost:27017'
 
-// 查询语句
-const sql = {
-	insert: 'insert into user(id, username, password) value(0,?,?)',
-  query: 'select * from user where username=?',
-  queryAll: 'select * from user',
-  update: 'update user set name=?, age=? where id=?',
-  delete: 'delete from user where id=?'
+const _checkUser = async (username) => {
+  await mongoClient.connect(dbUrl, (err, cli) => {
+    if (err) throw err
+    const db = cli.db('express')
+    const col = db.collection('user')
+    const cursor = col.find({
+      name: username
+    })
+    cursor.toArray((err, rows) => {
+      return rows.length>0
+    })
+    cli.close()
+  })
 }
 
-// 使用连接池，提升性能
-const poolConfig = Object.assign({ connectionLimit: 10 }, mysqlConfig)
-const pool = mysql.createPool(poolConfig)
-
-// 向前台返回JSON方法的简单封装
-const jsonWrite = function (res, ret) {
-	if(typeof ret === 'undefined') {
-		res.json({
-			code:'1',
-			msg: '操作失败'
-		})
-	} else {
-		res.json(ret)
-	}
-}
-
-router.post('/', (req, res, next) => {
-	// 获取前台页面传过来的参数
-	const { username, password } = req.body
-	console.log('param=', username, password)
-	pool.getConnection((err, connection) => {
-		if (err) throw err; // not connected!
-		connection.query(sql.query, [username], (err, result) => {
-			console.log('result', result)
-			if (result.length > 0) {
-				res.status(200).json({
-					msg: 'username exist'
-				})
-			}
-			else {
-				connection.query(sql.insert, [username, password], (err, result) => {
-					res.status(200).json({
-						msg: 'create'
-					})
-				})
-			}
-			// 释放连接 
-			connection.release()
-			// Handle error after the release.
-			if (err) throw err
-			// Don't use the connection here, it has been returned to the pool.
-		})
-	})
+router.post('/', async (req, res, next) => {
+  // 获取前台页面传过来的参数
+  const { username, password } = req.body
+  mongoClient.connect(dbUrl, (err, cli) => {
+    if (err) throw err
+    const db = cli.db('express')
+    const col = db.collection('user')
+    _checkUser(username).then((dat, a) => {
+      console.log('dat', dat, a)
+    })
+    res.status(200).end()
+    return
+    col.insertOne({
+      name: username,
+      password
+    })
+    cli.close()
+    res.status(200).end()
+    
+  })
 })
 
-router.get('/:userId', (req, res, next) => {
-	res.status(200).end()
+router.get('/', (req, res, next) => {
+  mongodb.connect('mongodb://localhost:27017', (err, client) => {
+    if (err) throw err
+    const db = client.db('express')
+    const collection = db.uesr.insert({
+      name: 'wangkj',
+      password: 'wangkj'
+    })
+    console.log(collection)
+    client.close()
+  })
+  res.status(200).end()
 })
 
-router.put('/', function(req, res, next) {
+router.put('/', function (req, res, next) {
   res.status(403).end()
 })
 
-router.delete('/', function(req, res, next) {
+router.delete('/', function (req, res, next) {
   res.status(403).end()
 })
 
