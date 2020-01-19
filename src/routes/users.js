87282c4e9440c6bc1bb70f1e02c0058ea6b3708f
wangkj <wangkaijie5@jd.com>
2020-01-19 15:16:31 +0800

    add func-register

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,8 +3,10 @@ const router = express.Router()
 const mongoClient = require('mongodb').MongoClient
 const dbUrl = 'mongodb://localhost:27017'
 
-const _checkUser = async (username) => {
-  await mongoClient.connect(dbUrl, (err, cli) => {
+router.post('/', async (req, res) => {
+  // 获取前台页面传过来的参数
+  const { username, password } = req.body
+  mongoClient.connect(dbUrl, (err, cli) => {
     if (err) throw err
     const db = cli.db('express')
     const col = db.collection('user')
@@ -12,53 +14,45 @@ const _checkUser = async (username) => {
       name: username
     })
     cursor.toArray((err, rows) => {
-      return rows.length>0
+      console.log('rows', rows)
+      if (rows.length > 0) {
+        res.status(400).json({
+          msg: `${username} has exist`
+        })
+      }
+      else {
+        col.insertOne({
+          name: username,
+          password
+        })
+        res.status(201).json({
+          msg: `${username} insert ok`
+        })
+      }
+      cli.close()
     })
-    cli.close()
   })
-}
+})
 
-router.post('/', async (req, res, next) => {
-  // 获取前台页面传过来的参数
-  const { username, password } = req.body
+router.get('/', (req, res) => {
   mongoClient.connect(dbUrl, (err, cli) => {
-    if (err) throw err
     const db = cli.db('express')
     const col = db.collection('user')
-    _checkUser(username).then((dat, a) => {
-      console.log('dat', dat, a)
+    const cursor = col.find()
+    cursor.toArray((err, rows) => {
+      res.status(200).json({
+        msg: `get data success.`,
+        data: rows
+      })
     })
-    res.status(200).end()
-    return
-    col.insertOne({
-      name: username,
-      password
-    })
-    cli.close()
-    res.status(200).end()
-    
   })
 })
 
-router.get('/', (req, res, next) => {
-  mongodb.connect('mongodb://localhost:27017', (err, client) => {
-    if (err) throw err
-    const db = client.db('express')
-    const collection = db.uesr.insert({
-      name: 'wangkj',
-      password: 'wangkj'
-    })
-    console.log(collection)
-    client.close()
-  })
-  res.status(200).end()
-})
-
-router.put('/', function (req, res, next) {
+router.put('/', function (req, res) {
   res.status(403).end()
 })
 
-router.delete('/', function (req, res, next) {
+router.delete('/', function (req, res) {
   res.status(403).end()
 })
 
