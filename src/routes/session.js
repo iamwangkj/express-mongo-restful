6303b2f57c1089e1d012a84e0bc694e20cdf4b4c
wangkj <wangkaijie5@jd.com>
2020-04-02 10:55:05 +0800

    fix conflict

--- src/routes/session.js
+++ src/routes/session.js
@@ -2,9 +2,6 @@ const express = require('express')
 const router = express.Router()
 const userModel = require('../models/users')
 
-<<<<<<< HEAD
-router.post('/', (req, res, next) => {
-=======
 const format = (msg, data) => {
   return {
     msg: msg || '',
@@ -13,29 +10,21 @@ const format = (msg, data) => {
 }
 
 router.post('/', async (req, res) => {
->>>>>>> 4d089b3d9a3910c78b5f03602388fd3540e68dfb
   const { username, password } = req.body
   const adventure = await userModel.findOne({ username })
   if (adventure) {
     const { password: _realPwd } = adventure
     if (password === _realPwd) {
       res.status(200).json(format('登录成功'))
-    }
-    else {
+    } else {
       res.status(200).json(format('密码错误'))
     }
-  }
-  else {
+  } else {
     res.status(200).json(format('用户未注册'))
   }
 })
 
-<<<<<<< HEAD
-router.delete('/', (req, res, next) => {
-=======
-
 router.delete('/', (req, res) => {
->>>>>>> 4d089b3d9a3910c78b5f03602388fd3540e68dfb
   const { username, password } = req.body
   res.status(200).json({
     msg: 'sign out'
